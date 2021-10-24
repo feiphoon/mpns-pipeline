0c09d2059f2d983dcef0f22ca00f90972386fa64
Fei Phoon <fei.phoon@gmail.com>
2021-10-24 21:31:31 +0100

    Fix and refactor mpns_v8_processing spark file

--- src/C_mpns_v8_processing/mpns_v8_processing.py
+++ src/C_mpns_v8_processing/mpns_v8_processing.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from typing import List
 
 from pyspark.sql import SparkSession, functions as f
-
 from pyspark.sql.dataframe import DataFrame
-from pyspark.sql.types import StructType
+from pyspark.sql.window import Window
+from pyspark.sql.types import StructType, BooleanType
 
 
 from input_schemas import (
@@ -13,6 +13,8 @@ from input_schemas import (
     MPNS_V8_SYNONYMS,
     MPNS_V8_NON_SCIENTIFIC_NAMES,
 )
+
+from output_schemas import OUTPUT_SCHEMA
 
 
 #  Monkeypatch in case I don't use Spark 3.0
@@ -31,6 +33,7 @@ def process_mpns_v8_raw(
 ) -> None:
     spark = SparkSession.builder.appName("process_mpns_v8_raw").getOrCreate()
 
+    # Load the three files into DataFrames
     plants_df = load_for_schema(
         spark,
         input_filepath=os.path.join(input_filepath, "medicinal_mpns_plants.csv"),
@@ -43,7 +46,6 @@ def process_mpns_v8_raw(
         schema=MPNS_V8_SYNONYMS,
         delimiter="|",
     )
-
     non_scientific_names_df = load_for_schema(
         spark,
         input_filepath=os.path.join(
@@ -53,6 +55,8 @@ def process_mpns_v8_raw(
         delimiter=";",
     )
 
+    # Filter plants DataFrame, join to non-scientific names,
+    # construct the name mappings from this.
     filtered_plants_df = filter_exclusions(
         df=plants_df,
         exclude_quality_rating=exclude_quality_rating,
@@ -63,7 +67,7 @@ def process_mpns_v8_raw(
         non_scientific_names_df,
         filtered_plants_df.name_id == non_scientific_names_df.plant_id,
         "left",
-    )
+    ).drop(non_scientific_names_df.name_id)
 
     plants_to_non_scientific_names_cols = [
         "name_id",
@@ -75,14 +79,12 @@ def process_mpns_v8_raw(
         *plants_to_non_scientific_names_cols
     )
 
-    plants_to_non_scientific_names_df = (
-        plants_to_non_scientific_names_df.withColumnRenamed(
-            "name_id", "full_scientific_name_id"
-        )
-        .withColumnRenamed("name", "non_scientific_name")
-        .withColumnRenamed("name_type", "non_scientific_name_type")
+    plants_to_non_scientific_names_df = construct_name_mappings_df(
+        df=plants_to_non_scientific_names_df, is_synonym_filetype=False
     )
 
+    # Filter synonyms DataFrame, join to non-scientific names,
+    # construct the name mappings from this.
     filtered_synonyms_df = filter_exclusions(
         df=synonyms_df,
         exclude_quality_rating=exclude_quality_rating,
@@ -93,7 +95,7 @@ def process_mpns_v8_raw(
         non_scientific_names_df,
         filtered_synonyms_df.acc_name_id == non_scientific_names_df.plant_id,
         "left",
-    )
+    ).drop(non_scientific_names_df.name_id)
 
     synonyms_to_non_scientific_names_cols = [
         "name_id",
@@ -105,16 +107,25 @@ def process_mpns_v8_raw(
         *synonyms_to_non_scientific_names_cols
     )
 
-    synonyms_to_non_scientific_names_df = (
-        synonyms_to_non_scientific_names_df.withColumnRenamed(
-            "name_id", "full_scientific_name_id"
-        )
-        .withColumnRenamed("name", "non_scientific_name")
-        .withColumnRenamed("name_type", "non_scientific_name_type")
+    synonyms_to_non_scientific_names_df = construct_name_mappings_df(
+        df=synonyms_to_non_scientific_names_df, is_synonym_filetype=True
     )
 
-    print("p nsn", plants_to_non_scientific_names_df.show(3, truncate=False))
-    print("s nsn", synonyms_to_non_scientific_names_df.show(3, truncate=False))
+    # Join both name mappings DataFrames for everything
+    all_name_mappings_df = plants_to_non_scientific_names_df.union(
+        synonyms_to_non_scientific_names_df
+    )
+
+    # Add a unique mapping_id - won't be deterministic with each run!
+    all_name_mappings_df = all_name_mappings_df.withColumn(
+        "mapping_id",
+        f.row_number().over(Window.orderBy("full_scientific_name")),
+    )
+
+    # Write name mappings to JSON files
+    write_name_mappings_to_json(
+        df=all_name_mappings_df, output_filepath=output_filepath
+    )
 
 
 def load_for_schema(
@@ -137,21 +148,39 @@ def filter_exclusions(
     # If both exclusion lists contain no items, short-circuit
     if (not exclude_quality_rating) and (not exclude_taxon_status):
         return df
-
     if exclude_quality_rating:
         for _quality_status in exclude_quality_rating:
             df = df.filter(f.col("quality_rating") != _quality_status)
-
     if exclude_taxon_status:
         for _taxon_status in exclude_taxon_status:
             df = df.filter(f.col("taxon_status") != _taxon_status)
-
     return df
 
 
+def construct_name_mappings_df(df: DataFrame, is_synonym_filetype: bool) -> DataFrame:
+    return (
+        df.withColumnRenamed("name_id", "full_scientific_name_id")
+        .withColumnRenamed("name", "non_scientific_name")
+        .withColumnRenamed("name_type", "non_scientific_name_type")
+        .withColumn("is_synonym", f.lit(is_synonym_filetype).cast(BooleanType()))
+    )
+
+
+def write_name_mappings_to_json(df: DataFrame, output_filepath: Path) -> None:
+    df.coalesce(1).write.format("json").option("schema", OUTPUT_SCHEMA).save(
+        output_filepath
+    )
+
+
+def write_process_metadata(output_filepath: Path) -> None:
+    pass
+
+
+mpns_raw_filepath = "data/mpns/sample_mpns_v8/"
+mpns_processed_filepath = "data/mpns/sample_mpns_v8/mpns_name_mappings"
 process_mpns_v8_raw(
-    input_filepath=Path("data/mpns/mpns_v8"),
-    output_filepath=Path("potato"),
+    input_filepath=mpns_raw_filepath,
+    output_filepath=mpns_processed_filepath,
     exclude_quality_rating=["L"],
     exclude_taxon_status=["Misapplied"],
 )

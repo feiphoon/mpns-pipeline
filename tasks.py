a73832c3c1d4025afbf4062be3febd01407324de
Fei Phoon <fei.phoon@gmail.com>
2021-12-01 00:50:45 +0000

    Add v5 processing task

--- tasks.py
+++ tasks.py
@@ -66,6 +66,18 @@ def mpns_v8_processing_run_v4(c):
 
 
 @task
+def mpns_v8_processing_run_v5(c):
+    c.run(
+        "docker run -v $(pwd):/job punchy/mpns-pipeline:0.1.0 \
+            src/A_mpns_v8_processing/mpns_v8_processing_v5.py \
+            --name 'mpns-pipeline-container'\
+                ;CONTAINER_ID=$(docker ps -lq)\
+                    ;docker cp `echo $CONTAINER_ID`:/data/processed/mpns data/processed/",
+        pty=True,
+    )
+
+
+@task
 def mpns_v8_name_mappings_analysis_v3(c):
     c.run(
         "docker run -v $(pwd):/job punchy/mpns-pipeline:0.1.0 \
@@ -110,6 +122,7 @@ ps.add_task(mpns_v8_processing_run_v1)
 ps.add_task(mpns_v8_processing_run_v2)
 ps.add_task(mpns_v8_processing_run_v3)
 ps.add_task(mpns_v8_processing_run_v4)
+ps.add_task(mpns_v8_processing_run_v5)
 ps.add_task(mpns_v8_name_mappings_analysis_v3, "mpns_name_mappings_analysis")
 ps.add_task(mpns_v8_name_relationships_analysis, "mpns_v8_name_relationships_analysis")
 ps.add_task(
